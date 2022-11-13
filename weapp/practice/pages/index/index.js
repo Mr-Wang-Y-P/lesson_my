c655b3de5fc569c3212cf2ce66b4fa25f675f908
Mr-Wang-Y-P <116936390+Mr-Wang-Y-P@users.noreply.github.com>
2022-11-13 17:19:08 +0800

    Add files via upload

---
+++ weapp/practice/pages/index/index.js
@@ -0,0 +1,45 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+   title:"梅赛德斯",
+   url:"../../images/1.jpg"
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    if (wx.getUserProfile) {
+      this.setData({
+        title:'保时捷911',
+        url:'https://img.pcauto.com.cn/images/upload/upc/tx/auto5/2208/19/c31/329675890_1660916687078_800x600.jpg'
+      },20000)
+    }
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
